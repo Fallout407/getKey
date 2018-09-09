820b0f485d8d93895803746e455859f49040b938
Fallout407 <38690894+Fallout407@users.noreply.github.com>
2018-09-09 08:20:40 +0800

    Create cmd.cpp

---
+++ lib/cmd.cpp
@@ -0,0 +1,111 @@
+#include <Windows.h>
+DWORD getKey(string key) {
+	if (key == "0") {
+		return 0x30;
+	}
+	if (key == "1") {
+		return 0x31;
+	}
+	if (key == "2") {
+		return 0x32;
+	}
+	if (key == "3") {
+		return 0x33;
+	}
+	if (key == "4") {
+		return 0x34;
+	}
+	if (key == "5") {
+		return 0x35;
+	}
+	if (key == "6") {
+		return 0x36;
+	}
+	if (key == "7") {
+		return 0x37;
+	}
+	if (key == "8") {
+		return 0x38;
+	}
+	if (key == "9") {
+		return 0x39;
+	}
+	if (key == "a") {
+		return 0x41;
+	}
+	if (key == "b") {
+		return 0x42;
+	}
+	if (key == "c") {
+		return 0x43;
+	}
+	if (key == "d") {
+		return 0x44;
+	}
+	if (key == "e") {
+		return 0x45;
+	}
+	if (key == "f") {
+		return 0x46;
+	}
+	if (key == "g") {
+		return 0x47;
+	}
+	if (key == "h") {
+		return 0x48;
+	}
+	if (key == "i") {
+		return 0x49;
+	}
+	if (key == "j") {
+		return 0x50;
+	}
+	if (key == "k") {
+		return 0x51;
+	}
+	if (key == "l") {
+		return 0x52;
+	}
+	if (key == "m") {
+		return 0x53;
+	}
+	if (key == "n") {
+		return 0x54;
+	}
+	if (key == "o") {
+		return 0x55;
+	}
+	if (key == "p") {
+		return 0x56;
+	}
+	if (key == "q") {
+		return 0x57;
+	}
+	if (key == "r") {
+		return 0x58;
+	}
+	if (key == "s") {
+		return 0x59;
+	}
+	if (key == "t") {
+		return 0x60;
+	}
+	if (key == "u") {
+		return 0x61;
+	}
+	if (key == "v") {
+		return 0x62;
+	}
+	if (key == "w") {
+		return 0x63;
+	}
+	if (key == "x") {
+		return 0x64;
+	}
+	if (key == "y") {
+		return 0x65;
+	}
+	if (key == "z") {
+		return 0x66;
+	}
+}
